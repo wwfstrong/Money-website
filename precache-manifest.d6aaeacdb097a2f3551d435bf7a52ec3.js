20469b9d6017b03dfd1857a90669fc0844d28a0e
wwf <865475679@qq.com>
2020-03-21 12:52:39 +0800

    deploy

---
+++ precache-manifest.d6aaeacdb097a2f3551d435bf7a52ec3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1138a013f2ba4135483",
+    "url": "/Money-website/css/app.5e0ac9eb.css"
+  },
+  {
+    "revision": "d7cf87301ab906c05650",
+    "url": "/Money-website/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "fb21156e7a811af6b10b748021127b1e",
+    "url": "/Money-website/index.html"
+  },
+  {
+    "revision": "b1138a013f2ba4135483",
+    "url": "/Money-website/js/app.91a715d9.js"
+  },
+  {
+    "revision": "d7cf87301ab906c05650",
+    "url": "/Money-website/js/chunk-vendors.175e7a9b.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/Money-website/manifest.json"
+  },
+  {
+    "revision": "fd9c73ce65356025bfff51b03f5ea1e3",
+    "url": "/Money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-website/robots.txt"
+  }
+]);
